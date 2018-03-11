18b47670735b5bcfaef636c0760393798c558937
carlosgbr <carlosgbr@gmail.com>
2018-03-11 16:55:58 -0600

    Update eb42_enum01.cpp

--- eb42_enum01.cpp
+++ eb42_enum01.cpp
@@ -25,5 +25,11 @@ switch (Figura) {
     default: cout << "Función no implementada"; break;
 }
 
+    //Revisamos el valor predeterminado que asigna el compilador a los enumeradores
+    
+    cout << endl<< endl << "Valor de Cuadro: " << Cuadro << endl;
+    cout << "Valor de Triangulo: " << Triangulo << endl;
+    cout << "Valor de Circulo: " << Circulo << endl;
+
 return 0;
 }
