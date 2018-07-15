0e06b63d8dee7d4cbd99e0f721d3732bf8f63cd0
carlosgbr <carlosgbr@gmail.com>
2018-07-15 16:11:27 -0500

    Create eb58_funciones_include1.cpp

---
+++ eb58_funciones_include1.cpp
@@ -0,0 +1,38 @@
+// eb58_funciones_include1.cpp
+// Este programa forma parte del tutorial de introducción al lenguaje C++
+// http://ehack.info/leguaje-c-introduccion/
+// Se ilustra la inclusión de librerías estándar, utilizamos string como ejemplo
+// 2018, Por http://about.me/carlosgbr
+// Versión 1
+// Compilado en https://www.onlinegdb.com/online_c++_compiler
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string entrada; 
+    
+    cout<<"Escribe una palabra: ";
+    cin >> entrada;
+    
+    cout << "Palabra actual: " << entrada << "\n";
+    cout << "Tamaño: " << entrada.size() << "\n";
+    cout << "Longitud: " << entrada.length() << "\n";
+    cout << "Capacidad: " << entrada.capacity() << "\n";
+    cout << "Tamaño Máximo: " << entrada.max_size() << "\n";
+    
+    
+    
+    entrada.clear();    //Limpiamos el contenido de la variable
+    
+    cout << "Palabra actual: " << entrada << "\n";
+    cout << "Tamaño: " << entrada.size() << "\n";
+    cout << "Longitud: " << entrada.length() << "\n";
+    cout << "Capacidad: " << entrada.capacity() << "\n";
+    cout << "Tamaño Máximo: " << entrada.max_size() << "\n";
+
+    return 0;
+}
