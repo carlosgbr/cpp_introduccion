e22f842025a51c47661916f38abad92846efe8a0
carlosgbr <carlosgbr@gmail.com>
2018-10-21 03:38:02 -0500

    Create eb60_funciones_proto.cpp

---
+++ eb60_funciones_proto.cpp
@@ -0,0 +1,49 @@
+// eb60_funciones_proto.cpp
+// Este programa forma parte del tutorial de introducción al lenguaje C++
+// http://ehack.info/leguaje-c-introduccion/
+// Se define un par de funciones que en el siguiente ejemplo 
+// eb61_funciones_proto2.cppse definirán como prototipos
+// 2018, Por http://about.me/carlosgbr
+// Versión 1
+// Compilado en https://www.onlinegdb.com/online_c++_compiler
+
+#include <iostream>
+
+using namespace std;
+
+int esMayor(int a, int b)
+{
+//Devuelve el número mayor entero de 2 números dados como parámetros
+if (a >= b) {
+    return a;
+} else {
+    return b;
+}
+
+return 0;
+}
+
+int esMenor(int a, int b)
+{
+//Devuelve el número mayor entero de 2 números dados como parámetros
+if (a <= b) {
+    return a;
+} else {
+    return b;
+}
+
+return 0;
+}
+
+int main()
+{
+    int num1, num2;
+    cout<<"Escribe un número entero: ";
+    cin >> num1;
+    cout<<"Escribe otro número entero: ";
+    cin >> num2;
+    cout<<"El mayor número introducido es: " << esMayor(num1, num2) << endl;
+    cout<<"El menor  número introducido es: " << esMenor(num1, num2);
+
+    return 0;
+}
