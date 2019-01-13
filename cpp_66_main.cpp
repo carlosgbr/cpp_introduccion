8005804b17c9d735cd481da3c4ac2e54a5dec2df
carlosgbr <carlosgbr@gmail.com>
2019-01-13 04:43:34 -0600

    Create cpp_66_main.cpp

---
+++ cpp_66_main.cpp
@@ -0,0 +1,11 @@
+// cpp_66_main.cpp
+// Declaración de la función main
+// Además declara el programa mínimo que podemos hacer en C++
+// 2019, Por http://about.me/carlosgbr
+// Versión 1
+// Compilado en https://www.onlinegdb.com/online_c++_compiler
+
+int main( int argc, const char* argv[] )
+{
+    return 0;
+}
