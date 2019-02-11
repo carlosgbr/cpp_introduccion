b4b9a7163df6bf2441a983444044de4f3a1b122c
carlosgbr <carlosgbr@gmail.com>
2019-02-11 02:40:04 -0600

    Update cpp_70_sobrecargaFunciones.cpp

--- cpp_70_sobrecargaFunciones.cpp
+++ cpp_70_sobrecargaFunciones.cpp
@@ -1,8 +1,8 @@
 // cpp_70_sobrecargaFunciones.cpp
 // Se ilustra la sobrecarga de funciones
 // 2019, Por http://about.me/carlosgbr
-// VersiC3n 1
-// Revisa todo el cC3digo del tutoral en: https://github.com/carlosgbr/
+// Versión 1
+// Revisa todo el código del tutoral en: https://github.com/carlosgbr/
 // Compilado en https://www.onlinegdb.com/online_c++_compiler
 
 #include <iostream>
