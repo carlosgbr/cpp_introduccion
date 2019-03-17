74894aac0ce61d8a2775bc382d312bb787329144
carlosgbr <carlosgbr@gmail.com>
2019-03-17 05:30:52 -0600

    Update cpp_72_pseudoaleatorio.cpp

--- cpp_72_pseudoaleatorio.cpp
+++ cpp_72_pseudoaleatorio.cpp
@@ -4,26 +4,15 @@
 // Versión 1
 // Revisa todo el código del tutoral en: https://github.com/carlosgbr/
 // Compilado en https://www.onlinegdb.com/online_c++_compiler
-
 #include <iostream>
 #include <cstdlib>
-
 using namespace std;
-
 int main()
 {
     int a;
     
     a = rand();             // Genera un valor entre 0 y RAND_MAX
     cout << a << endl;
-    a = rand() % 100;       // Genera un valor entre 0 y 99
-    cout << a << endl;
-    a = rand() % 100 + 1;   // Genera un valor entre 1 y 100
-    cout << a << endl;      
-    a = rand() % 50 + 20;   // Genera un valor entre 20 y 69
-    cout << a << endl;    
     cout << "RAND_MAX para este equipo tiene un valor de: " << RAND_MAX;
-    
-
     return 0;
 }
