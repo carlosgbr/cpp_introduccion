4e93d9d9878f9a6fad4698ba8a3fa8d5ce7d308b
carlosgbr <carlosgbr@gmail.com>
2019-05-19 23:33:34 -0500

    Create cpp_78_factorial2.cpp

---
+++ cpp_78_factorial2.cpp
@@ -0,0 +1,27 @@
+// cpp_78_factorial2.cpp
+// Variación de una función recursiva para calcular el factorial de un número
+// 2019, Por Geof en Sololearn
+// Versión 1
+// Revisa todo el código del tutoral en: https://github.com/carlosgbr/
+// Compilado en https://www.onlinegdb.com/online_c++_compiler
+
+#include <iostream>
+
+using namespace std;
+
+long long  factorial (int n, int r) {
+      if (n == 0) {
+          return r;
+      }
+      else {
+          return factorial (n - 1, n * r);
+      }
+ }
+ 
+ int main()
+ {
+    int n; 
+    cin >> n; 
+    cout<< "Factorial de  "<< n <<" es " << factorial(n,1) << endl;
+    return 0;
+ }
