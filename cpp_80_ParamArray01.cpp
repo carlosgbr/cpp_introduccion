d4f5e4cb7790b855cef7b90bdedaea43220d7ad9
carlosgbr <carlosgbr@gmail.com>
2019-06-23 02:31:03 -0500

    Update cpp_80_ParamArray01.cpp

--- cpp_80_ParamArray01.cpp
+++ cpp_80_ParamArray01.cpp
@@ -1,12 +1,7 @@
 // eb80_ParamArray01.cpp
 // Este programa forma parte del tutorial de introducción al lenguaje C++
 // http://ehack.info/leguaje-c-introduccion/
-// Se ilustra la declaración el uso de funciones y estructuras de control
-// El programa tiene un "error" intencional, las fórmulas de Conversión
-// de grados centígrados a Kelvin y de Kelvin a grados centígrados están 
-// invertidas, esto para ilustrar en la lección una de las ventajas de las 
-// funciones: Se modifica el código únicamente en la sección que tiene el error
-// sin tener que tocar el código en el programa principal.
+// Se ilustra el paso de Arrays como parámetros en funciones en C++
 // 2019, Por http://about.me/carlosgbr
 // Versión 1
 // Compilado en https://www.onlinegdb.com/online_c++_compiler
